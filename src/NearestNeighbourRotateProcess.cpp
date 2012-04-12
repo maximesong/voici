534b300def3172db3e413806243eadec26ca562b
Wenhao Song <song@linux.(none)>
2012-04-12 11:12:59 +0800

    make the image to the center after rotate

--- src/NearestNeighbourRotateProcess.cpp
+++ src/NearestNeighbourRotateProcess.cpp
@@ -18,11 +18,12 @@ QImage NearestNeighbourRotateProcess::generateNewImage(const QImage &image)
 {
 	double sinA = sin(counter_clock_rotate_angle / 180 * PI);
 	double cosA = cos(counter_clock_rotate_angle / 180 * PI);
-	double rx = image.width() / 2;
-	double ry = image.width() / 2;
 
 	int new_length = sqrt(image.width() * image.width() + 
 			      image.height() *image.height());
+
+	double rx = new_length / 2;
+	double ry = new_length / 2;
 
 	double x_diff = (new_length - image.width()) / 2;
 	double y_diff = (new_length - image.height()) / 2;
@@ -36,17 +37,15 @@ QImage NearestNeighbourRotateProcess::generateNewImage(const QImage &image)
 	const uchar *ps;
 	uchar *pd;
 
-	uchar transparent[] = { 255, 255, 255, 255 };
+	uchar transparent[] = { 255, 255, 255, 0 };
  
 	double const_x = -rx * cosA + ry * sinA + rx;
 	double const_y = -rx * sinA - ry * cosA + ry;
 
 	for (int j = 0; j < new_image.height(); ++j) {
 		for (int i = 0; i < new_image.height(); ++ i) {
-			int x = i * cosA - j * sinA + const_x;
-			int y = i * sinA + j * cosA + const_y;
-			if (i == 0 && j == 0) {
-				cout << const_x << "\t" << const_y << "\t" << x << "\t" << y << "\t" << endl;}
+			int x = i * cosA - j * sinA + const_x - x_diff;
+			int y = i * sinA + j * cosA + const_y - y_diff;
 			if (image.valid(x, y))
 				ps = src + (x + y * image.width()) * bytes;
 			else
